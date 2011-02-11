07fb20bdefd6bacec67772db4badc1d3a64ef288
Gonçalo Silva <goncalossilva@gmail.com>
2011-02-11 19:19:39 +0000

    fix config include in rails

--- rails/init.rb
+++ rails/init.rb
@@ -12,9 +12,7 @@ unless File.exists?(kaltura_yml)
   raise RuntimeError, "Unable to find \"config/kaltura.yml\" file."
 end
 
-
-KalturaFu.config = YAML.load_file(kaltura_yml).symbolize_keys[RAILS_ENV.to_sym]
-
+KalturaFu.config = YAML.load_file(kaltura_yml)[RAILS_ENV].symbolize_keys
 
 unless[:partner_id,:subpartner_id,:administrator_secret].all? {|key| KalturaFu.config.key?(key)}
   raise RuntimeError, "Kaltura config requires :partner_id, :subpartner_id,"+
